d86e4c3942b6da7e3b7bdc9bcf02d37670c3c827
Aarne Welschlau <aarne.welschlau@ratepay.com>
2015-04-10 18:30:56 +0200

    clean up to re-initiate with new namespace in v3.0.0

--- js/ratepay/rppayment.js
+++
@@ -1,5 +0,0 @@
-function alterVatId(method)
-{
-    document.getElementById('vatIdLabel_' + method).style.display = 'none';
-    document.getElementById('vatIdForm_' + method).style.display = 'inline-block';
-}
